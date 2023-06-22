5accd3a5734fdfcc55a50a82a3a92839d46f97e1
XDark-Warrior <129609960+XDark-Warrior@users.noreply.github.com>
2023-06-22 18:17:36 +0600

    Update mailer.php

--- mailer.php
+++ mailer.php
@@ -1,121 +1,45 @@
 <?php
-// SMTP server details
-$smtpServer = 'mail.teamxdarkwarrior.x10.mx';
-$smtpUsername = 'support@teamxdarkwarrior.x10.mx';
-$smtpPassword = '@XDWar999';
-$smtpPort = 587;
+require 'path/to/PHPMailer/src/PHPMailer.php';
+require 'path/to/PHPMailer/src/SMTP.php';
+require 'path/to/PHPMailer/src/Exception.php';
 
-// Sender and recipient details
-$senderEmail = 'sender@example.com';
-$senderName = 'Sender Name';
-$recipientEmail = 'recipient@example.com';
-$recipientName = 'Recipient Name';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-// Email content
-$subject = 'Hello from SMTP';
-$message = 'This is a test email.';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $senderName = $_POST['s_name'];
+    $senderEmail = $_POST['s_email'];
+    $subject = $_POST['subject'];
+    $recipientEmail = $_POST['r_email'];
+    $message = $_POST['message'];
 
-// Create the headers
-$headers = "From: $senderName <$senderEmail>\r\n";
-$headers .= "Reply-To: $senderName <$senderEmail>\r\n";
-$headers .= "MIME-Version: 1.0\r\n";
-$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+    $mail = new PHPMailer(true);
 
-// Construct the email
-$email = [
-    'to' => "$recipientName <$recipientEmail>",
-    'subject' => $subject,
-    'message' => $message,
-    'headers' => $headers
-];
+    try {
+        // SMTP Configuration
+        $mail->isSMTP();
+        $mail->Host = 'mail.teamxdarkwarrior.x10.mx'; // Replace with your SMTP server address
+        $mail->SMTPAuth = true;
+        $mail->Username = 'support@teamxdarkwarrior.x10.mx'; // Replace with your email address
+        $mail->Password = '@XDWar999'; // Replace with your email password
+        $mail->SMTPSecure = 'tls'; // Replace with the encryption type (tls or ssl)
+        $mail->Port = 587; // Replace with the appropriate SMTP port
 
-// Open a connection to the SMTP server
-$smtpConnection = fsockopen($smtpServer, $smtpPort, $errorNumber, $errorMessage, 10);
-if (!$smtpConnection) {
-    die("Failed to connect to the SMTP server: $errorMessage");
+        // Sender and recipient details
+        $mail->setFrom($senderEmail, $senderName);
+        $mail->addAddress($recipientEmail);
+
+        // Email content
+        $mail->isHTML(true);
+        $mail->Subject = $subject;
+        $mail->Body = $message;
+
+        // Send the email
+        $mail->send();
+        echo 'Email sent successfully.';
+    } catch (Exception $e) {
+        echo 'Email sending failed. Error: ' . $mail->ErrorInfo;
+    }
 }
-
-// Read the welcome message from the server
-$smtpResponse = fgets($smtpConnection);
-if (substr($smtpResponse, 0, 3) != '220') {
-    die("Error: Unexpected SMTP server response: $smtpResponse");
-}
-
-// Send the EHLO command to initiate the SMTP conversation
-fputs($smtpConnection, "EHLO example.com\r\n");
-$smtpResponse = fgets($smtpConnection);
-if (substr($smtpResponse, 0, 3) != '250') {
-    die("Error: EHLO command failed: $smtpResponse");
-}
-
-// Send the STARTTLS command to switch to a secure connection
-fputs($smtpConnection, "STARTTLS\r\n");
-$smtpResponse = fgets($smtpConnection);
-if (substr($smtpResponse, 0, 3) != '220') {
-    die("Error: STARTTLS command failed: $smtpResponse");
-}
-
-// Establish a secure connection
-if (!stream_socket_enable_crypto($smtpConnection, true, STREAM_CRYPTO_METHOD_TLS_CLIENT)) {
-    die("Error: Failed to establish a secure connection to the SMTP server");
-}
-
-// Authenticate with the server
-fputs($smtpConnection, "AUTH LOGIN\r\n");
-$smtpResponse = fgets($smtpConnection);
-if (substr($smtpResponse, 0, 3) != '334') {
-    die("Error: AUTH LOGIN command failed: $smtpResponse");
-}
-
-fputs($smtpConnection, base64_encode($smtpUsername) . "\r\n");
-$smtpResponse = fgets($smtpConnection);
-if (substr($smtpResponse, 0, 3) != '334') {
-    die("Error: Failed to send username: $smtpResponse");
-}
-
-fputs($smtpConnection, base64_encode($smtpPassword) . "\r\n");
-$smtpResponse = fgets($smtpConnection);
-if (substr($smtpResponse, 0, 3) != '235') {
-    die("Error: Failed to send password: $smtpResponse");
-}
-
-// Send the MAIL FROM command
-fputs($smtpConnection, "MAIL FROM: <$senderEmail>\r\n");
-$smtpResponse = fgets($smtpConnection);
-if (substr($smtpResponse, 0, 3) != '250') {
-    die("Error: MAIL FROM command failed: $smtpResponse");
-}
-
-// Send the RCPT TO command
-fputs($smtpConnection, "RCPT TO: <$recipientEmail>\r\n");
-$smtpResponse = fgets($smtpConnection);
-if (substr($smtpResponse, 0, 3) != '250') {
-    die("Error: RCPT TO command failed: $smtpResponse");
-}
-
-// Send the DATA command to start sending the email content
-fputs($smtpConnection, "DATA\r\n");
-$smtpResponse = fgets($smtpConnection);
-if (substr($smtpResponse, 0, 3) != '354') {
-    die("Error: DATA command failed: $smtpResponse");
-}
-
-// Send the email headers
-fputs($smtpConnection, $email['headers']);
-
-// Send the email body
-fputs($smtpConnection, "\r\n" . $email['message']);
-
-// Send the terminating sequence
-fputs($smtpConnection, "\r\n.\r\n");
-$smtpResponse = fgets($smtpConnection);
-if (substr($smtpResponse, 0, 3) != '250') {
-    die("Error: Failed to send email: $smtpResponse");
-}
-
-// Send the QUIT command to close the connection
-fputs($smtpConnection, "QUIT\r\n");
-fclose($smtpConnection);
-
-echo 'Email sent successfully.';
 ?>
